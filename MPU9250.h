cd68c5c234454eed52bf29c6360a72d7214beb60
nini302 <81171483+nini302@users.noreply.github.com>
2022-08-25 01:39:25 +0800

    Add files via upload

---
+++ MPU9250.h
@@ -0,0 +1,79 @@
+/*
+ * MPU925.h
+ *
+ *  Created on: 23 ��� 2018 �.
+ *      Author: Max
+ */
+
+#ifndef MPU925_H_
+#define MPU925_H_
+
+
+
+#ifndef UTIL_MPU9250_CONFIG_H_
+#define UTIL_MPU9250_CONFIG_H_
+
+#define MPU9250_SPI			hspi2
+#define	MPU9250_CS_GPIO		GPIOB
+#define	MPU9250_CS_PIN		GPIO_PIN_12
+
+#endif /* UTIL_MPU9250_CONFIG_H_ */
+#include "main.h"
+
+
+typedef enum GyroRange_ {
+	GYRO_RANGE_250DPS = 0,
+	GYRO_RANGE_500DPS,
+	GYRO_RANGE_1000DPS,
+	GYRO_RANGE_2000DPS
+} GyroRange;
+
+typedef enum AccelRange_ {
+	ACCEL_RANGE_2G = 0,
+	ACCEL_RANGE_4G,
+	ACCEL_RANGE_8G,
+	ACCEL_RANGE_16G
+} AccelRange;
+
+typedef enum DLPFBandwidth_ {
+	DLPF_BANDWIDTH_184HZ = 0,
+	DLPF_BANDWIDTH_92HZ,
+	DLPF_BANDWIDTH_41HZ,
+	DLPF_BANDWIDTH_20HZ,
+	DLPF_BANDWIDTH_10HZ,
+	DLPF_BANDWIDTH_5HZ
+} DLPFBandwidth;
+
+typedef enum SampleRateDivider_ {
+	LP_ACCEL_ODR_0_24HZ = 0,
+	LP_ACCEL_ODR_0_49HZ,
+	LP_ACCEL_ODR_0_98HZ,
+	LP_ACCEL_ODR_1_95HZ,
+	LP_ACCEL_ODR_3_91HZ,
+	LP_ACCEL_ODR_7_81HZ,
+	LP_ACCEL_ODR_15_63HZ,
+	LP_ACCEL_ODR_31_25HZ,
+	LP_ACCEL_ODR_62_50HZ,
+	LP_ACCEL_ODR_125HZ,
+	LP_ACCEL_ODR_250HZ,
+	LP_ACCEL_ODR_500HZ
+} SampleRateDivider;
+
+uint8_t MPU9250_Init();
+/* read the data, each argiment should point to a array for x, y, and x */
+void MPU9250_GetData(int16_t* AccData, int16_t* MagData, int16_t* GyroData);
+
+/* sets the sample rate divider to values other than default */
+void MPU9250_SetSampleRateDivider(SampleRateDivider srd);
+/* sets the DLPF bandwidth to values other than default */
+void MPU9250_SetDLPFBandwidth(DLPFBandwidth bandwidth);
+/* sets the gyro full scale range to values other than default */
+void MPU9250_SetGyroRange(GyroRange range);
+/* sets the accelerometer full scale range to values other than default */
+void MPU9250_SetAccelRange(AccelRange range);
+
+#endif /* MPU925_H_ */
+
+
+
+
